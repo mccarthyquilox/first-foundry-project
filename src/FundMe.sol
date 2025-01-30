e4b90b84edaaa36bbd7c3224cce05e4577a74316
mccarthy <mulch2356@gmail.com>
2025-01-30 21:31:57 +0100

    Initial commit

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -4,7 +4,7 @@
 pragma solidity ^0.8.18;
 // 2. Imports
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import { AggregatorV3Interface } from "lib/lib/chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 // 3. Interfaces, Libraries, Contracts
